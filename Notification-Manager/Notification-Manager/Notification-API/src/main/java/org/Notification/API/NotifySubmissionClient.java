afc0a4576a496c927de72187edd0cc6057408709
nikchett <nikchett@iu.edu>
2017-10-15 12:38:42 -0400

    Added rabbit mq

--- Notification-Manager/Notification-Manager/Notification-API/src/main/java/org/Notification/API/NotifySubmissionClient.java
+++ Notification-Manager/Notification-Manager/Notification-API/src/main/java/org/Notification/API/NotifySubmissionClient.java
@@ -7,25 +7,76 @@ import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
 
+
+import org.apache.thrift.TDeserializer;
+import org.apache.thrift.TSerializer;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+
 public class NotifySubmissionClient {
+	private final static String QUEUE_NAME = "notify";
+
 	public static void main(String[] args) {
 
-		  try {
-		   TTransport transport;
-
-		   transport = new TSocket("localhost", 9091);
-		   transport.open();
-
-		   TProtocol protocol = new TBinaryProtocol(transport);
-		   NotifySubmissionService.Client client = new NotifySubmissionService.Client(protocol);
-
-		   System.out.println(client.notifySubmission("1001"));
-
-		   transport.close();
-		  } catch (TTransportException e) {
-		   e.printStackTrace();
-		  } catch (TException x) {
-		   x.printStackTrace();
-		  }
+//		  try {
+//		   TTransport transport;
+//
+//		   transport = new TSocket("localhost", 9091);
+//		   transport.open();
+//
+//		   TProtocol protocol = new TBinaryProtocol(transport);
+//		   NotifySubmissionService.Client client = new NotifySubmissionService.Client(protocol);
+//
+//		   System.out.println(client.notifySubmission("1001"));
+//
+//		   transport.close();
+//		  } catch (TTransportException e) {
+//		   e.printStackTrace();
+//		  } catch (TException x) {
+//		   x.printStackTrace();
+//		  }
+		  
+		    try {
+		    	   
+		        //Create a connection factory
+		        ConnectionFactory factory = new ConnectionFactory();
+		        //Set the host to the location of the RabbitMQ server
+		        factory.setHost("localhost");
+		        //Open a new connection
+		        Connection connection = factory.newConnection();
+		        //Channel is the abstraction for interacting with a queue
+		        Channel channel = connection.createChannel();
+		        //Create the Queue if it does not exist
+		        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+		       
+		        //Create a new instance of the Thrift Serializer
+		      //  TSerializer serializer = new TSerializer();
+		        //Serialize our domain object to a byte array
+		      //  byte[] payload = serializer.serialize("hello");
+		        
+		        String message  = "hello" ;
+		        
+		          //Publish message on the queue using our domain
+		          //object as the message (or payload)
+		          channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+		        
+		          //Print a friendly message
+		          System.out.println(
+		             " [x] Sent ");
+		        
+		       
+		        //Close the channel
+		        channel.close();
+		        //Close the connection
+		        connection.close();
+		       
+		      } catch (Exception e) {
+		        //Dump any exception to the console
+		        e.printStackTrace();
+		      }
 		 }
 }
--- Notification-Manager/Notification-Manager/Notification-API/src/main/java/org/Notification/API/NotifySubmissionServer.java
+++ Notification-Manager/Notification-Manager/Notification-API/src/main/java/org/Notification/API/NotifySubmissionServer.java
@@ -1,27 +1,71 @@
 package org.Notification.API;
 
 import org.apache.thrift.transport.TServerSocket;
+import com.rabbitmq.client.*;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
 import org.apache.thrift.transport.TServerTransport;
+
+import java.io.IOException;
+
+import org.Notification.Sender.MailNotification;
+import org.apache.thrift.TDeserializer;
 import org.apache.thrift.server.TServer;
 import org.apache.thrift.server.TServer.Args;
 import org.apache.thrift.server.TSimpleServer;
 
 public class NotifySubmissionServer {
 	public static void StartsimpleServer(NotifySubmissionService.Processor<NotifySubmissionServerHandler> processor) {
-		  try {
-		   TServerTransport serverTransport = new TServerSocket(9091);
-		   TServer server = new TSimpleServer(
-		     new Args(serverTransport).processor(processor));
+//		  try {
+//		   TServerTransport serverTransport = new TServerSocket(9091);
+//		   TServer server = new TSimpleServer(
+//		     new Args(serverTransport).processor(processor));
+//
+//		   // Use this for a multithreaded server
+//		   // TServer server = new TThreadPoolServer(new
+//		   // TThreadPoolServer.Args(serverTransport).processor(processor));
+//
+//		   System.out.println("Starting the simple server...");
+//		   server.serve();
+//		  } catch (Exception e) {
+//		   e.printStackTrace();
+//		  }
+		try {
+			   
+		      //Create a connection factory
+		      ConnectionFactory factory = new ConnectionFactory();
+		      //Set the host to the location of the RabbitMQ server
+		      factory.setHost("localHost");
+		      //Open a new connection
+		      Connection connection = factory.newConnection();
+		      //Channel is the abstraction for interacting with a queue
+		      Channel channel = connection.createChannel();
+		      //It's possible the Queue doesn't already exist (a producer
+		      //may not have been instantiated yet).  Create the queue if
+		      //it doesn't exist
+		      channel.queueDeclare("notify", false, false, false, null);
+		      //Comfort logging
+		      System.out.println(
+		         " [*] Waiting for messages. To exit press CTRL+C");
+		      System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-		   // Use this for a multithreaded server
-		   // TServer server = new TThreadPoolServer(new
-		   // TThreadPoolServer.Args(serverTransport).processor(processor));
-
-		   System.out.println("Starting the simple server...");
-		   server.serve();
-		  } catch (Exception e) {
-		   e.printStackTrace();
-		  }
+		      Consumer consumer = new DefaultConsumer(channel) {
+		        @Override
+		        public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+		            throws IOException {
+		          String message = new String(body, "UTF-8");
+		          System.out.println(" [x] Received '" + message + "'");
+		        }
+		       
+		      };
+		      channel.basicConsume("notify", true, consumer);
+		      (new MailNotification()).sendMail("1001", (new NotifySubmissionServerHandler()).getAdminId());
+		       
+		    } catch(Exception e){
+		      //Dump any error to the console
+		      e.printStackTrace();
+		    }
 		 }
 		 
 		 public static void main(String[] args) {
